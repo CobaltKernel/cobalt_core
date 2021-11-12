f99afe5d517e19121a3209141384d5ea8af21639
NPEX42 <NPEX42@gmail.com>
2021-11-12 13:56:04 +0000

    Rexported boot_fn! as entrypoint!

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
 #![no_std]
 
+pub use cobalt_boot::boot_fn as entrypoint;
+
+use cobalt_boot::hook_panic_fn;
+use core::{panic::PanicInfo};
+
+#[doc(hidden)]
+pub fn _panic(_: &PanicInfo) -> ! {
+    loop {};
+}
